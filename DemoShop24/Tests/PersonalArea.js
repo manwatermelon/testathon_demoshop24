d587f8f5c7925f06c5f6ce3033ab6929ac2847b3
Alexander Arbuzov <alexander.arbuzov@ctco.lv>
2018-10-02 14:25:19 +0300

    Added selector, added tests

--- DemoShop24/Tests/PersonalArea.js
+++ DemoShop24/Tests/PersonalArea.js
@@ -1,6 +1,7 @@
 import {user_NOK} from "./mocks/user.mocks";
 import RegistrationPage from 'pages/registrationPage';
 import PageHeader from "./pages/pageHeader";
+import { Selector } from 'testcafe';
 
 const pageHeader = new PageHeader();
 const page = new RegistrationPage();
@@ -12,20 +13,25 @@ fixture`User Registration`
             await t
                 .click(pageHeader.myAccountElement)
                 .click(pageHeader.registerElement);
-        }
-    );
+        });
 
 test('should have mandatory fields', async t => {
     await t
-        .typeText(page.nameInput, user_NOK.firstName)
-        .typeText(page.lastNameInput, user_NOK.secondName)
-        .typeText(page.mailInput, user_NOK.email)
-        .typeText(page.telephoneInput, user_NOK.phoneNumber)
-        .typeText(page.passwordInput, user_NOK.password)
-        .typeText(page.passwordConfirmationInput, user_NOK.password)
-        .click(page.confirmCheckbox);
+        .click(page.continueButton);
+
+    await t.expect(Selector('.text-danger').count > 0);
 });
 
-test('should validate password equality'), async t => {
-
-}
+test('should validate password equality', async t => {
+    await t
+        .typeText(page.nameInput, user_OK.firstName)
+        .typeText(page.lastNameInput, user_OK.secondName)
+        .typeText(page.mailInput, user_OK.email)
+        .typeText(page.telephoneInput, user_OK.phoneNumber)
+        .typeText(page.passwordInput, user_OK.password)
+        .typeText(page.passwordConfirmationInput, '1')
+        .click(page.confirmCheckbox)
+        .click(page.continueButton);
+    const passwordWarningElem = page.passwordConfirmationInput.nextSibling(Selector('.text-danger'));
+    await t.expect(passwordWarningElem.innerHTML).eql('Password confirmation does not match password!');
+});
