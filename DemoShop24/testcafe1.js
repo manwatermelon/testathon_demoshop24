b7e0aa7af4b146f3e1919e306c095f8139fb0565
Alexander Arbuzov <alexander.arbuzov@ctco.lv>
2018-10-02 10:22:05 +0300

    First test passed

---
+++ DemoShop24/testcafe1.js
@@ -0,0 +1,10 @@
+﻿import { Selector } from 'testcafe';
+
+fixture`TestFixture`
+    .page `http://demoshop24.com`;
+
+test('Check Label HTML', async t => {
+    await t
+        .click('#top-links > ul > li:nth-child(3) > a')
+        .navigateTo('http://demoshop24.com/index.php?route=checkout/cart');
+});
