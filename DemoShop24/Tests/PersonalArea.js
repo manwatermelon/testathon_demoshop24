856ef575dcd6224f83cb29c03f057724b41cedde
Alexander Arbuzov <alexander.arbuzov@ctco.lv>
2018-10-02 13:25:07 +0300

    Added button Continue and NOK data

--- DemoShop24/Tests/PersonalArea.js
+++ DemoShop24/Tests/PersonalArea.js
@@ -8,11 +8,15 @@ fixture`User Registration`
 
 test('should have mandatory fields', async t => {
     await t
-        .typeText(page.nameInput,user_NOK.firstName )
+        .typeText(page.nameInput, user_NOK.firstName)
         .typeText(page.lastNameInput, user_NOK.secondName)
-        .typeText(page.mailInput, '')
-        .typeText(page.telephoneInput, '')
-        .typeText(page.passwordInput, '')
-        .typeText(page.passwordConfirmationInput, '')
-        .click(page.confirmCheckbox)
+        .typeText(page.mailInput, user_NOK.email)
+        .typeText(page.telephoneInput, user_NOK.phoneNumber)
+        .typeText(page.passwordInput, user_NOK.password)
+        .typeText(page.passwordConfirmationInput, user_NOK.password)
+        .click(page.confirmCheckbox);
 });
+
+test('should validate password equality'), async t => {
+
+}
